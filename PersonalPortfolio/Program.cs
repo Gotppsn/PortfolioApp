478a63ab3ce4f14c0b328008f31851fd9154e8e1
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-04-15 20:32:13 +0700

    update

--- PersonalPortfolio/Program.cs
+++ PersonalPortfolio/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PersonalPortfolio;
 using PersonalPortfolio.Services;
-using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ProjectService>();
-builder.Services.AddScoped<IJSRuntime>(sp => sp.GetRequiredService<IJSRuntime>());
 
 await builder.Build().RunAsync();
--- PersonalPortfolio/obj/Debug/net9.0/PersonalPortfolio.AssemblyInfo.cs
+++ PersonalPortfolio/obj/Debug/net9.0/PersonalPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4349a916cab0eca4f0670ea3cb1eb0b3f091ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf31054f09b8729534e54da5edd9f46cb1d55717")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
