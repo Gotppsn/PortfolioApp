d833deaf3ad49567633a51f34a24ad68c61ab442
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-04-15 19:55:41 +0700

    update

--- PersonalPortfolio/obj/Debug/net9.0/PersonalPortfolio.AssemblyInfo.cs
+++ PersonalPortfolio/obj/Debug/net9.0/PersonalPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f1f204de6b88001805877bf4ee329151bb5de6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e6e98097aac392f9d5bdbdc70cba5ade0e0eab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
